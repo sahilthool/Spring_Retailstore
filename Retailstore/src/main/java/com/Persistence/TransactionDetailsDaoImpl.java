75be0cd29f51eeb6762f95ebdd4f582c8887dcd3
Tejaswini <guttulatejaswini@gmail.com>
2022-08-17 20:11:33 +0530

    Transaction_details dao

--- Retailstore/src/main/java/com/Persistence/TransactionDetailsDaoImpl.java
+++ Retailstore/src/main/java/com/Persistence/TransactionDetailsDaoImpl.java
@@ -9,108 +9,73 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
 import com.Bean.Transaction_Details;
+import com.Persistence.helper.TransactionDetailsRowMapper;
 
-
+@Repository
 public class TransactionDetailsDaoImpl implements TransactionDetailsDao {
 
-	@Override
-	public void showalltransactionDetails() {            
-		try( Connection connection=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/retailstore", "root", "wiley");
-				Statement statement=connection.createStatement();	) {
+	
+	private JdbcTemplate jdbcTemplate;
 
-			ResultSet resultSet= statement.executeQuery("SELECT * FROM transaction_details");
-			
-			while(resultSet.next()) {
-				int Transaction_id=resultSet.getInt("transaction_id");
-				int Item_Id=resultSet.getInt("item_Id");
-				int quantity=resultSet.getInt("quantity");
+	@Autowired
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+	
+	
+
+	@Override
+	public void showalltransactionDetails() {    
+		String query="SELECT * FROM transaction_details";
+		List<Transaction_Details> list=jdbcTemplate.query(query, new TransactionDetailsRowMapper());
+		
+		System.out.println("Transaction_id    Item_Id    quantity");
+			for(Transaction_Details td:list){
+				int Transaction_id=td.getTransaction_ID();
+				int Item_Id=td.getItem_ID();
+	        int quantity=td.getQuantity();
 	
 				System.out.println(Transaction_id+" "+Item_Id+" "+quantity);
 			}
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
 
 	}
 
 
 	@Override
 	public boolean deletetransactionDetail(int id) {
-		try (Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/retailstore", "root",
-				"wiley");
-				PreparedStatement preparedStatement = connection
-						.prepareStatement("DELETE FROM transaction_details where transaction_Id=?");) {
+		
+		String query = "DELETE FROM Transaction_Details where transaction_ID=?";
 
-			preparedStatement.setInt(1,id);
-
-			int rows = preparedStatement.executeUpdate();
-
-			if (rows>0) {
-				//System.out.println("Deleted");
-				return true;
-			}
-			else {
-				return false;
-			}
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return false;
-
+		int rows = jdbcTemplate.update(query, id);
+        if(rows>0)
+		return true;
+        else 
+        	return false;
 	}
 
 	@Override
 	public boolean addtransactionDetail(Transaction_Details trans_details) {
-		int rows = 0;
-		try (Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/retailstore", "root",
-				"wiley");
-				PreparedStatement preparedStatement = connection
-						.prepareStatement("INSERT INTO transaction_details values(?,?,?)");) {
+		String query = "INSERT INTO Transaction_Details values(?,?,?)";
 
-			preparedStatement.setInt(1, trans_details.getTransaction_ID());
-			preparedStatement.setInt(2, trans_details.getItem_ID());
-			preparedStatement.setInt(3, trans_details.getQuantity());			
-
-			rows = preparedStatement.executeUpdate();
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		if (rows>0)
-		   return true;
-		else
-			return false;
+		int rows = jdbcTemplate.update(query,trans_details.getTransaction_ID(),trans_details.getItem_ID(),trans_details.getQuantity() );
+       if(rows>0)
+		return true;
+       else
+    	   return false;
 	}
 
 
 	@Override
 	public List<Transaction_Details> searchTransactionDetails(int transid) {
-		List<Transaction_Details> list = new ArrayList<Transaction_Details>();
 		
-		try (Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/retailstore", "root",
-				"wiley");
-				PreparedStatement preparedStatement = connection
-						.prepareStatement("SELECT * FROM transaction_details where transaction_id=?");) {
-
-			preparedStatement.setInt(1,transid);
-
-			ResultSet resultSet = preparedStatement.executeQuery();
-
-			while(resultSet.next()) {
-				int id1 = resultSet.getInt("transaction_Id");
-				int id2 = resultSet.getInt("Item_Id");
-				int quantity = resultSet.getInt("quantity");
-				
-				list.add(new Transaction_Details(id1,id2,quantity) );
-				//System.out.println("fnn"+list);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		String query="SELECT * FROM transaction_details where transaction_id=?";
+		List<Transaction_Details> list=jdbcTemplate.query(query, new TransactionDetailsRowMapper(),transid);
+		
 		return list;
 		
 	}
@@ -118,54 +83,27 @@ public class TransactionDetailsDaoImpl implements TransactionDetailsDao {
 
 	@Override
 	public boolean deleteitemfromcart(int item_id) {
-		try (Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/retailstore", "root",
-				"wiley");
-				PreparedStatement preparedStatement = connection
-						.prepareStatement("DELETE FROM transaction_details where item_id=?");) {
+		
+		String query = "DELETE FROM Transaction_Details where item_id=?";
 
-			preparedStatement.setInt(1,item_id);
-
-			int rows = preparedStatement.executeUpdate();
-
-			if (rows>0) {
-				//System.out.println("Deleted");
-				return true;
-			}
-			else {
-				return false;
-			}
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return false;
+		int rows = jdbcTemplate.update(query, item_id);
+        if(rows>0)
+		return true;
+        else 
+        	return false;
 	}
 
 
 	@Override
 	public boolean updateitemquantity(int item_id, int quantity) {
-		try (Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/retailstore", "root",
-				"wiley");
-				PreparedStatement preparedStatement = connection
-						.prepareStatement("UPDATE transaction_details SET quantity=? where item_id=?");) {
-			
-			preparedStatement.setInt(1,quantity);
-			preparedStatement.setInt(2,item_id);	
+		
+		String query = "UPDATE transaction_details SET quantity=? where item_id=?";
 
-			int rows = preparedStatement.executeUpdate();
-
-			if (rows>0) {
-				//System.out.println("Deleted");
-				return true;
-			}
-			else {
-				return false;
-			}
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return false;
+		int rows = jdbcTemplate.update(query, quantity,item_id);
+        if(rows>0)
+		return true;
+        else 
+        	return false;
 	}
 
 }
