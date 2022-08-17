ad84d913fe806305e47614eab069a1578971ce78
“sahilthool” <sahilthool997@gmail.com>
2022-08-17 19:01:43 +0530

    customer spring jdbc

--- Retailstore/src/main/java/com/Controller/LoginController.java
+++ Retailstore/src/main/java/com/Controller/LoginController.java
@@ -17,32 +17,29 @@ public class LoginController {
 	
 	@Autowired
 	private customerService customerService;
-	//private UserService userService;
+	
 
 	@RequestMapping("/")
 	public ModelAndView loginPageController() {
 		return new ModelAndView("Login", "command", new Customer());
 	}
 
-	@RequestMapping("/login")
-	public ModelAndView loginController(@ModelAttribute Customer customer,HttpSession session) {
-		
-		ModelAndView modelAndView=new ModelAndView();
+//	@RequestMapping("/login")
+//	public ModelAndView loginController(@ModelAttribute Customer customer,HttpSession session) {
+//		
+//		ModelAndView modelAndView=new ModelAndView();
 //		if (customerService.searchCustomer(customer)) {
 //			modelAndView.addObject("customer", customer);  //user object added at request scope
 //			session.setAttribute("customer", customer);
 //			modelAndView.setViewName("index");
+//		}		
+//		else {
+//			modelAndView.addObject("message", "Invalid Credentials");
+//			modelAndView.addObject("command", new Customer());
+//			modelAndView.setViewName("Login");
 //		}
-		if(true) {
-			modelAndView.setViewName("index");
-		}
-		else {
-			modelAndView.addObject("message", "Invalid Credentials");
-			modelAndView.addObject("command", new Customer());
-			modelAndView.setViewName("Login");
-		}
-		return modelAndView;
-	}
+//		return modelAndView;
+//	}
 	
 
 }
--- Retailstore/src/main/java/com/Persistence/CustomerDaoImp.java
+++ Retailstore/src/main/java/com/Persistence/CustomerDaoImp.java
@@ -6,112 +6,72 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import com.Bean.Customer;
+import com.Persistence.helper.CustomerRowMapper;
 
+@Repository
 public class CustomerDaoImp implements CustomerDao {
+	
+	private JdbcTemplate jdbcTemplate;
 
-	@Override
-	public void showCustomer() {
-		try( Connection connection=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/retailstore", "root", "wiley");
-				Statement statement=connection.createStatement();	) {
-
-			ResultSet resultSet= statement.executeQuery("SELECT * FROM customer");
-			
-			while(resultSet.next()) {
-				int customer_id=resultSet.getInt("customer_Id");
-				String customer_name=resultSet.getString("customer_Name");
-				String user_name=resultSet.getString("User_Name");
-				String password=resultSet.getString("passwords");
-								
-				System.out.println(customer_id +" "+customer_name+" "+user_name+" "+password);
-			}
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
+	@Autowired
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
 	}
 
+
+
 	@Override
-	public boolean deleteCustomer(int id) {
-		try (Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/retailstore", "root",
-				"wiley");
-				PreparedStatement preparedStatement = connection
-						.prepareStatement("DELETE FROM customer where customer_Id=?");) {
+	public int deleteCustomer(int id) {
+		
+		String query = "DELETE FROM customer where customer_Id=?";
 
-			preparedStatement.setInt(1,id);
+		int rows = jdbcTemplate.update(query, id);
 
-			ResultSet resultSet = preparedStatement.executeQuery();
-
-			if (resultSet.next()) {
-				System.out.println("Customer Data Deleted");
-				return true;
-			}
-			else {
-				return false;
-			}
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return false;
+		return rows;		
 		
 	}
 
 	@Override
 	public Customer searchCustomer(int id) {
-		Customer cust=null;
-		try (Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/retailstore", "root",
-				"wiley");
-				PreparedStatement preparedStatement = connection
-						.prepareStatement("SELECT * FROM customer where customer_Id=?");) {
-
-			preparedStatement.setInt(1,id);
-
-			ResultSet resultSet = preparedStatement.executeQuery();
-            
-			if (resultSet.next()) {
-				int id1 = resultSet.getInt("customer_Id");
-				String customer_Name=resultSet.getString("customer_Name");
-				String userName=resultSet.getString("User_Name");
-				String password=resultSet.getString("passwords");
-				cust=new Customer(id1,customer_Name,userName,password);
-				return cust;
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return cust;
 		
+		Customer customer=null;
+		try {
+		String query="SELECT * FROM customer where customer_Id=?";
+		customer=jdbcTemplate.queryForObject(query, new CustomerRowMapper(), id);
+		}
+		catch(EmptyResultDataAccessException ex) {
+			return customer;
+		}
+		return customer;		
 		
 	}
 
 
 	@Override
-	public boolean addCustomer(Customer customer) {
-		int rows = 0;
-		try (Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/retailstore", "root",
-				"wiley");
-				PreparedStatement preparedStatement = connection
-						.prepareStatement("INSERT INTO Customer values(?,?,?,?)");) {
-
-			preparedStatement.setInt(1, customer.getCustomer_ID());
-			preparedStatement.setString(2, customer.getCustomer_Name());
-			preparedStatement.setString(3, customer.getUser_Name());
-			preparedStatement.setString(4, customer.getPassword());
-			
-
-			rows = preparedStatement.executeUpdate();
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		if (rows>0)
-		   return true;
-		else
-			return false;
+	public int addCustomer(Customer customer) {
 		
+		String query = "INSERT INTO Customer values(?,?,?,?)";
+
+		int rows = jdbcTemplate.update(query, customer.getCustomer_ID(), customer.getCustomer_Name(), customer.getUser_Name(), customer.getPassword());
+
+		return rows;		
+		
+	}
+
+	@Override
+	public List<Customer> getAllCustomer() {
+		String query="SELECT * FROM Customer";
+		List<Customer> cusList=jdbcTemplate.query(query, new CustomerRowMapper());
+		
+		return cusList;
 	}
 
 
